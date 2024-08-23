de57ca4570280c6d45721fbab0e499e80bd4eea7
kenzor1979 <jonasschuh@hotmail.com>
2024-08-22 22:56:36 -0300

    adjustments

--- mscloudgateway/src/main/java/io/github/kenzor1979/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/kenzor1979/mscloudgateway/MscloudgatewayApplication.java
@@ -22,9 +22,9 @@ public class MscloudgatewayApplication {
 	public RouteLocator routes(RouteLocatorBuilder builder){
 		return builder
 				.routes()
-					.route(r -> r.path("/clients/**").uri("lb://msclients"))
-					.route(r -> r.path("/cards/**").uri("lb://mscards"))
-				    .route(r -> r.path("/credit-assessor/**").uri("lb://mscreditassessor"))
+				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.route(r -> r.path("/cards/**").uri("lb://mscards"))
+				.route(r -> r.path("/credit-assessor/**").uri("lb://mscreditassessor"))
 				.build();
 	}
 
